9129f2d2a78bd4be71525881c9f57b936f59e508
koyoungwoong <yuko@hallym.ac.kr>
2023-05-08 22:55:13 +0900

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import data from './data.js';
+import { useState } from 'react';
+import { Routes, Route, Link } from 'react-router-dom'
 
 function App() {
+  let [animal] = useState(data);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/Home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/Home">Home</Nav.Link>
+            <Nav.Link href="/Features">Features</Nav.Link>
+            <Nav.Link href="/Pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Routes>
+        <Route path="/Home" element={ <div>Home page</div> } />
+        <Route path="/Features" element={ <div>Feature page</div> } />
+        <Route path="/Pricing" element={ <div>Price page</div> } />
+     </Routes>
+
+      <div className="container">
+        <div className="row">
+          <Animal animal={animal[0]} i={1} />
+          <Animal animal={animal[1]} i={2}/>
+          <Animal animal={animal[2]} i={3}/>
+        </div>
+      </div>
+
     </div>
+
   );
 }
+
+function Animal(props){
+  return (
+    <div className="col-md-4">
+      <img src={'http://210.115.229.18/00' + props.i +'.jpg'} width="80%" />
+      <h4>{props.animal.title}</h4>
+      <p>{props.animal.where}</p>
+      <p>{props.animal.age}</p>
+  </div>
+  )
+}
+
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,25 @@
+let data = [
+    {
+      id : 0,
+      title : "Deer",
+      where : "Born in Korea",
+      age : 4
+    },
+  
+    {
+      id : 1,
+      title : "Red Fox",
+      where : "Born in China",
+      age : 11
+    },
+  
+    {
+      id : 2,
+      title : "Tiger",
+      where : "Born in Japan",
+      age : 10
+    },
+  
+  ] 
+
+export default data
